07b67b2889b7c318a759ecdbe9ae6f69fb430abf
Vladut-Cotfas <vladutgabriel.cotfas@p4b.ro>
2024-05-26 14:08:09 +0300

    Added design changes Chat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'vet-plus';
+
+  isChatOpen = false;
+
+  toggleChat(): void {
+    this.isChatOpen = !this.isChatOpen;
+  }
 }
